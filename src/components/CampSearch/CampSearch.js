988f1e0a151b125a98ef1975deddfac759ef01a6
Sam Preston <sam.preston11@gmail.com>
2022-02-01 12:16:54 -0800

    CampSearch fetches data from NPS API.

--- src/components/CampSearch/CampSearch.js
+++ src/components/CampSearch/CampSearch.js
@@ -1,11 +1,27 @@
-// import XMLParser from 'react-xml-parser';
+import React, { useEffect } from "react";
 import styles from "./CampSearch.module.css";
+import Card from "../ui/Card/Card";
 
 const CampSearch = (props) => {
+  useEffect(() => {
+    const fetchCamps = async () => {
+      try {
+        const response = await fetch(`https://developer.nps.gov/api/v1/campgrounds?limit=627&api_key=${process.env.REACT_APP_NPS_API_KEY}`);
+        
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching camps: ", error);
+      }
+    };
+
+    fetchCamps();
+  }, []);
+
   return (
-    <div className={styles.CampSearch}>
+    <Card className={styles.search}>
       <p>Woop there it is.</p>
-    </div>
+    </Card>
   );
 };
 
