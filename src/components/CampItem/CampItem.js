12cf943c9317edfc3037156bce9420bedf0a4797
Sam Preston <sam.preston11@gmail.com>
2022-02-02 12:41:46 -0800

    CampItem styling.

--- src/components/CampItem/CampItem.js
+++ src/components/CampItem/CampItem.js
@@ -1,14 +1,21 @@
 import styles from "./CampItem.module.css";
 import Button from "../ui/Button/Button";
 
+// Displays individual camp details to CampsList. Passes selected camp info to App.
 const CampItem = (props) => {
+  // Lift selected camp info to App
   const viewCampHandler = () => {
     props.onViewCamp(props.camp);
   };
+
   return (
     <div className={styles.container}>
-      {props.camp.name}
-      <Button onClick={viewCampHandler} alt={false}>View</Button>
+      <div className={styles.content}>{props.camp.name}</div>
+      <div>
+        <Button onClick={viewCampHandler} alt={false}>
+          View
+        </Button>
+      </div>
     </div>
   );
 };
