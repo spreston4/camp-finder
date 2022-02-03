5435981f2bade02ad8378829c148a7f7a3084a8f
Sam Preston <sam.preston11@gmail.com>
2022-02-03 10:50:20 -0800

    Restyled Header.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,14 +3,11 @@ import styles from "./Header.module.css";
 
 const Header = () => {
   return (
-    <React.Fragment>
-        <div className={styles.title}>
-          <h1>Camp Finder</h1>
-        </div>
-        <div className={styles.front}></div>
-        <div className={styles.middle}></div>
-        <div className={styles.back}></div>
-    </React.Fragment>
+    <div className={styles.container}>
+      <div className={styles.small}>
+        <h1>Camp Finder</h1>
+      </div>
+    </div>
   );
 };
 
