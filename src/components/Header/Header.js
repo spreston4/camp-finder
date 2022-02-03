9835c8baa21421d04dcfced4810a4422db759b8a
Sam Preston <sam.preston11@gmail.com>
2022-02-03 11:11:52 -0800

    Added resize on scroll effect to header.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import styles from "./Header.module.css";
 
 const Header = () => {
+  const [isSmall, setIsSmall] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", () => 
+      setIsSmall(window.pageYOffset > 200)
+      );
+    }
+  }, [])
+
+  const titleClasses = `${styles.title} ${isSmall ? styles.small : ''}`;
+
   return (
     <div className={styles.container}>
-      <div className={styles.small}>
+      <div className={titleClasses}>
         <h1>Camp Finder</h1>
       </div>
     </div>
