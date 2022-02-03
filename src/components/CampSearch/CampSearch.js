6fd9b79d4503389b866f10ee31506df331daa85a
Sam Preston <sam.preston11@gmail.com>
2022-02-02 16:59:12 -0800

    Validates user input in CampSearch using useInput hook.

--- src/components/CampSearch/CampSearch.js
+++ src/components/CampSearch/CampSearch.js
@@ -1,34 +1,66 @@
 import { useState } from "react";
+import useInput from "../../hooks/use-input";
 import styles from "./CampSearch.module.css";
 import Card from "../ui/Card/Card";
 import Button from "../ui/Button/Button";
 
 const CampSearch = (props) => {
-  // TODO: Setting state twice - look at implementing context
-  const [searchTerms, setSearchTerms] = useState();
+  const {
+    value: enteredState,
+    isValid: enteredStateIsValid,
+    hasError: stateHasError,
+    valueChangeHandler: stateChangeHandler,
+    inputBlurHandler: stateBlurHandler,
+    resetHandler: stateResetHandler,
+  } = useInput((value) =>
+    value
+      .trim()
+      .match(
+        /^([Aa][LKSZRAEPlkszraep]|[Cc][AOTaot]|[Dd][ECec]|[Ff][LMlm]|[Gg][AUau]|[Hh][Ii]|[Ii][ADLNadln]|[Kk][SYsy]|[Ll][Aa]|[Mm][ADEHINOPSTadehinopst]|[Nn][CDEHJMVYcdehjmvy]|[Oo][HKRhkr]|[Pp][ARWarw]|[Rr][Ii]|[Ss][CDcd]|[Tt][NXnx]|[Uu][Tt]|[Vv][AITait]|[Ww][AIVYaivy])$/gim
+      )
+  );
+
+  let formIsValid = false;
+
+  if (enteredStateIsValid) {
+    formIsValid = true;
+  }
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
-    // TODO: Add validation
-    props.onUpdateSearch(searchTerms);
+
+    if (!formIsValid) {
+      return;
+    }
+
+    props.onUpdateSearch(enteredState);
   };
 
-  const inputChangeHandler = (event) => {
-    setSearchTerms(event.target.value);
-  };
+  const stateClasses = stateHasError ? `${styles.invalid}` : "";
 
   return (
     <Card className={styles.container}>
       <form onSubmit={formSubmissionHandler}>
-        <label htmlFor="state-input">Search for campgrounds by State ID!</label>
-        <input
-          onChange={inputChangeHandler}
-          name="state-input"
-          id="state-input"
-          type="text"
-        />
-        <Button alt={false}>Submit</Button>
+        <div className={stateClasses}>
+          <label htmlFor="state-input">
+            Search for campgrounds by State ID!
+          </label>
+          <input
+            onChange={stateChangeHandler}
+            onBlur={stateBlurHandler}
+            value={enteredState}
+            name="state-input"
+            id="state-input"
+            type="text"
+          />
+        </div>
+        <Button alt={false} type="submit">
+          Submit
+        </Button>
       </form>
+      {stateHasError && (
+        <p className={styles.error}>Enter a valid state abbreviation: XX</p>
+      )}
     </Card>
   );
 };
