2423133cdf02978214a7fdbeab592315255beef1
Sam Preston <sam.preston11@gmail.com>
2022-02-08 15:01:30 -0800

    Displays available campsite info to CampView through CampSiteNumbers component.

---
+++ src/components/CampSiteNumbers/CampSiteNumbers.js
@@ -0,0 +1,46 @@
+import React from "react";
+import styles from "./CampSiteNumbers.module.css";
+
+const CampSiteNumbers = (props) => {
+  return (
+    <React.Fragment>
+      <h3>Campsites</h3>
+      <div className={styles.details}>
+        <div className={styles.individual}>
+          <p className={styles.title}>Total Campsites</p>
+          <p>{props.sites.totalSites}</p>
+        </div>
+        <div className={styles.individual}>
+          <p className={styles.title}>First Come, First Serve</p>
+          <p>{props.firstCome}</p>
+        </div>
+        <div className={styles.individual}>
+          <p className={styles.title}>Reservable</p>
+          <p>{props.reservable}</p>
+        </div>
+        <div className={styles.individual}>
+          <p className={styles.title}>Tent Only</p>
+          <p>{props.sites.tentOnly}</p>
+        </div>
+        <div className={styles.individual}>
+          <p className={styles.title}>RV Only</p>
+          <p>{props.sites.rvOnly}</p>
+        </div>
+        <div className={styles.individual}>
+          <p className={styles.title}>Group</p>
+          <p>{props.sites.group}</p>
+        </div>
+        <div className={styles.individual}>
+          <p className={styles.title}>Horse</p>
+          <p>{props.sites.horse}</p>
+        </div>
+        <div className={styles.individual}>
+          <p className={styles.title}>Electrical Hookups</p>
+          <p>{props.sites.electricalHookups}</p>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default CampSiteNumbers;
--- src/components/CampView/CampView.js
+++ src/components/CampView/CampView.js
@@ -1,5 +1,6 @@
 import styles from './CampView.module.css';
 import WeatherDisplay from '../WeatherDisplay/WeatherDisplay';
+import CampSiteNumbers from '../CampSiteNumbers/CampSiteNumbers';
 import Button from '../ui/Button/Button';
 
 const CampView = (props) => {
@@ -8,8 +9,10 @@ const CampView = (props) => {
         <div className={styles.container}>
             <div className={styles.content}>
                <h2>{props.camp.name}</h2>
+               <p>{props.camp.description}</p>
+               <CampSiteNumbers sites={props.camp.campsites} firstCome={props.camp.numberOfSitesFirstComeFirstServe} reservable={props.camp.numberOfSitesReservable} />
             </div>
-            <div>
+            <div className={styles.weather}>
                 <WeatherDisplay latitude={props.camp.latitude} longitude={props.camp.longitude} />
             </div>
             <div className={styles.controls}>
