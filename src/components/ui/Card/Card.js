d83f47e31410114a4d57052ba6a4d8341f5e5d97
Sam Preston <sam.preston11@gmail.com>
2022-02-14 10:32:38 -0800

    Removed unused components & custom hooks.

--- src/components/ui/Card/Card.js
+++
@@ -1,9 +0,0 @@
-import styles from "./Card.module.css";
-
-// Reusable card component - accepts parent component styling
-const Card = (props) => {
-  const cardClasses = `${styles.card} ${props.className}`;
-  return <div className={cardClasses}>{props.children}</div>;
-};
-
-export default Card;
